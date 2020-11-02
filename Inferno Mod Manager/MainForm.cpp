70a3b213b2fb9d927f9c67adf67f562e9d36e11e
Matt <39469614+KosmicShovel@users.noreply.github.com>
2020-11-02 17:42:17 -0600

    Winform funny work

--- Inferno Mod Manager/MainForm.cpp
+++ Inferno Mod Manager/MainForm.cpp
@@ -1,2 +1,15 @@
 #include "MainForm.h"
 
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+[STAThread]
+int main()
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	InfernoModManager::MainForm form;
+	Application::Run(%form);
+	return 0;
+}
