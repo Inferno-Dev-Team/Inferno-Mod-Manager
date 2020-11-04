c7b0234a1c8ddd2f2af5f0a5b2818efc6782e548
Matt <39469614+KosmicShovel@users.noreply.github.com>
2020-11-03 19:25:52 -0600

    Grabbing game from registry info done

--- Inferno Mod Manager/MainForm.cpp
+++ Inferno Mod Manager/MainForm.cpp
@@ -1,14 +1,25 @@
 #include "MainForm.h"
-
+#include "SupportedGames.cpp"
 #include "Injection.h"
+#include <map>
 
 using namespace System;
 using namespace System::Windows::Forms;
 
+std::map<long long, const char*> InfernoModManager::Games::GameInfo;
+
+void init()
+{
+	auto inf = std::map<long long, const char*>();
+	inf.insert( std::pair<long long, const char*>(960090, "BloonsTD6"));
+
+	InfernoModManager::Games::GameInfo = inf;
+}
 
 [STAThread]
 int main()
 {
+	init();
 	InfernoModManager::Injection::injectDLL("BloonsTD6", "D:\\Inferno-Mod-Manager\\LoggingDLL\\x64\\Release\\LoggingDLL.dll");
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
--- Inferno Mod Manager/SupportedGames.cpp
+++ Inferno Mod Manager/SupportedGames.cpp
@@ -1,9 +1,75 @@
-namespace InfernoManager
+#include <map>
+#include <msclr\marshal.h>
+
+namespace InfernoModManager
 {
+
 	class Games
 	{
 		public:
-			const __int64 BloonsTD6AppID = 960090;
-			const char* BloonsTD6ProcessName = "BloonsTD6";
+			static std::map<long long, const char*> GameInfo;
+
+			static bool IsGameInstalled(const __int64 id)
+			{
+				System::Object^ installed = Microsoft::Win32::Registry::GetValue(Microsoft::Win32::Registry::CurrentUser + "\\Software\\Valve\\Steam\\Apps\\" + id, "Installed", nullptr);
+
+				if (!installed)
+					return 0;
+				return (int)installed == 1;
+			}
+
+			static System::String^ GetSteamDir()
+			{
+				Microsoft::Win32::Registry::GetValue(Microsoft::Win32::Registry::CurrentUser + "\\Software\\Valve\\Steam", "SteamPath", nullptr);
+			}
+
+			static System::String^ GetGameDir(const __int64 id)
+			{
+				System::String^ steamDir = GetSteamDir();
+				if (System::String::IsNullOrEmpty(steamDir))
+					throw gcnew System::Exception("Steam Directory invalid!");
+
+				if (!IsGameInstalled(id))
+					throw gcnew System::Exception("You don't have that game installed!");
+
+				System::Collections::Generic::List<System::String^>^ allPossible = gcnew System::Collections::Generic::List<System::String^>();
+				System::String^ config = steamDir + "\\steamapps\\libraryfolders.vdf";
+				allPossible->Add(config);
+				array<System::String^>^ configFile = System::IO::File::ReadAllLines(config);
+				for each (System::String^ configPart in configFile)
+				{
+					System::Text::RegularExpressions::Regex^ reg = gcnew System::Text::RegularExpressions::Regex("\".?\"");
+					for (int i = 0; i < reg->Matches(configPart)->Count; ++i)
+					{
+						int tmp;
+
+						if (i == 1)
+							allPossible->Add(reg->Matches(configPart)[i]->Value->ToString());
+						if (i == 0)
+							if (!int::TryParse(reg->Matches(configPart)[i]->Value->ToString(), tmp))
+								break;
+					}
+				}
+
+				System::String^ gameName;
+
+				for each (std::pair<long long, const char*> pair in GameInfo)
+				{
+					if (pair.first == id)
+					{
+						gameName = msclr::interop::marshal_as<System::String^>(pair.second);
+					}
+				}
+
+				for each (System::String^ possible in allPossible)
+				{
+					System::String^ gameFolder = possible + "\\steamapps\\common\\" + gameName;
+					if (System::IO::Directory::Exists(gameFolder))
+					{
+						return gameFolder;
+					}
+				}
+				return "ERROR CANT FIND GAME";
+			}
 	};
 }
