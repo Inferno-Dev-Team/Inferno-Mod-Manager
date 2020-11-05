512e088c6e3f5605727b418d1bc3b5c40a916034
Matt <39469614+KosmicShovel@users.noreply.github.com>
2020-11-05 15:54:46 -0600

    start api

---
+++ Inferno Api/Inferno Api/Inferno_Main.cs
@@ -0,0 +1,9 @@
+﻿using MelonLoader;
+
+namespace Inferno_Api {
+    class Main : MelonMod {
+        public override void OnApplicationStart() {
+
+        }
+    }
+}
---
+++ Inferno Api/Inferno Api/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Inferno")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Inferno")]
+[assembly: AssemblyCopyright("Copyright © KosmicShovel 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(true)]
+[assembly: Guid("7136208C-B74F-46A2-9F89-9DACAD099836")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
